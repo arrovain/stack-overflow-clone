3d3f1e3e6f31abeb6e945281d847d34f8ffc9c3d
arrovain <arrovain@gmail.com>
2024-09-22 15:57:48 +0300

    question resource

--- backend/app/Http/Requests/UpdateQuestionRequest.php
+++ backend/app/Http/Requests/UpdateQuestionRequest.php
@@ -11,7 +11,7 @@ class UpdateQuestionRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,8 @@ class UpdateQuestionRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'title' => 'required|string|max:255|unique:questions,title,'.$this->question->id,
+            'body' => 'required'
         ];
     }
 }
--- backend/app/Http/Resources/QuestionResource.php
+++ backend/app/Http/Resources/QuestionResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -13,7 +14,19 @@ class QuestionResource extends JsonResource
      * @return array<string, mixed>
      */
     public function toArray(Request $request): array
-    {
-        return parent::toArray($request);
+    {   
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'slug' => $this->slug,
+            'body' => $this->body,
+            'viewCount' => $this->viewCount.' '.Str::plural("view", $this->viewCount),
+            'score' => $this->score.' '.Str::plural("vote", $this->score),
+            'answerCount' => $this->answers()->count().' '.Str::plural("answer", $this->answers()->count()),
+            'answers' => AnswerResource::collection($this->whenLoaded('answers')),
+            'created_at' => $this->created_at->diffForHumans(),
+            'user' => UserResource::make($this->whenLoaded('user')),
+            'tags' => $this->tags
+        ];
     }
 }
